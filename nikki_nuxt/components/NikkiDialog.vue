b77e2945d19f03abbe2c019c5fdde040523acf55
masamune <kenjiii534@gmail.com>
2022-11-06 07:29:10 +0900

    nikkidialog.vue を追加

---
+++ nikki_nuxt/components/NikkiDialog.vue
@@ -0,0 +1,28 @@
+  <template></template>
+
+
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  mounted() {
+    const date = new Date()
+  },
+  methods: {
+    /**
+     * apiからもらう日付データを調整する
+     */
+    dateMilliSecondsToString(second: number): string {
+      const date = new Date(second * 1000)
+      return date.toLocaleDateString('ja-japanese')
+    },
+  },
+})
+</script>
