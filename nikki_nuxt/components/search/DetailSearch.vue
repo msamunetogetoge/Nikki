b1cdc09fcd1c30f7d4107b6b490dc2a991aa5b54
masamune <kenjiii534@gmail.com>
2022-11-27 20:12:30 +0900

    詳細検索コンポーネントの作成

--- nikki_nuxt/components/search/DetailSearch.vue
+++ nikki_nuxt/components/search/DetailSearch.vue
@@ -1,16 +1,38 @@
 <template>
   <v-card>
     <v-card-title> 詳細検索 </v-card-title>
-    <v-form>
+    <v-card-text>
       <v-row>
-        <v-col> から </v-col>
-        <v-col> まで </v-col>
+        <v-col>
+          <date-picker :date-picker-label="'から'" @dateUpdate="setFromDate" />
+        </v-col>
+        <v-col>
+          <date-picker :date-picker-label="'まで'" @dateUpdate="setToDate" />
+        </v-col>
       </v-row>
       <v-row>
-        <v-col> 最低 </v-col>
-        <v-col> 最高 </v-col>
+        <v-col>
+          <v-text-field
+            v-model="goodnessMin"
+            inputmode="numeric"
+            type="number"
+            label="良さ最低"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="goodnessMax"
+            inputmode="numeric"
+            type="number"
+            label="良さ最高"
+          ></v-text-field>
+        </v-col>
       </v-row>
-    </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn @click="search"> 検索</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -18,30 +40,43 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import DatePicker from '../common/DatePicker.vue'
 
 export default defineComponent({
   name: 'DetailSearchComponent',
+  components: {
+    DatePicker,
+  },
 
   data() {
     return {
-      text: '',
+      goodnessMin: 0,
+      goodnessMax: 10,
     }
   },
   watch: {
-    text(val) {
-      // 親に更新されたtextを渡す
-      this.$emit('updateText', val)
+    goodnessMin(val: number) {
+      this.goodnessMin = Number(val)
+      this.$emit('updateGoodnessMin', this.goodnessMin)
+    },
+    goodnessMax(val: number) {
+      this.goodnessMax = Number(val)
+      this.$emit('updateGoodnessMax', this.goodnessMax)
     },
   },
   mounted() {},
   methods: {
+    setFromDate(fromDate: Date) {
+      const fromDateUTCString = fromDate.toUTCString()
+      this.$emit('updateFromDate', fromDateUTCString)
+    },
+    setToDate(toDate: Date) {
+      const toDateUTCString = toDate.toUTCString()
+      this.$emit('updateToDate', toDateUTCString)
+    },
     // Nikki検索のフラグを送る
     search() {
       this.$emit('search')
-    },
-    // 詳細画面を開くフラグを送る
-    showDetail() {
-      this.$emit('showDetail')
     },
   },
 })
